c50714b367454493f84dc6a21210f3b88bc0fe0b
Irene Santamaría de Pedraza <Irene@MacBook-Pro-de-Irene.local>
2014-08-24 22:30:00 +0200

    added test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Irene Santamaría de Pedraza on 24/08/14.
+//  Copyright (c) 2014 Sergio Magaña. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Irene Santamaría de Pedraza on 24/08/14.
+//  Copyright (c) 2014 Sergio Magaña. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
